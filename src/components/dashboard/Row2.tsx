252f093ebafd8761eb54a84888e9921ce2d96bd9
Sebastian madero <rockmadero@gmail.com>
2023-09-13 15:52:24 -0500

    cambios menores

--- src/components/dashboard/Row2.tsx
+++ src/components/dashboard/Row2.tsx
@@ -3,10 +3,10 @@ import { DashboardBox } from '..';
 export const Row2 = () => {
   return (
     <>
+      <DashboardBox gridArea="d"></DashboardBox>
       <DashboardBox gridArea="e"></DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
       <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
     </>
   );
 };
